5a73b23410628c66d1bcf9a350d5ee0523f3580a
Samadrita Hazra <rajeshhazra879@l@gmail.com>
2026-02-17 13:17:46 +0530

    Switch to PostgreSQL

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,11 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./preppilot.db"  # simple & safe for now
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
